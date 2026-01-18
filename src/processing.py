b674cb8c81e2d924ccb7f1317320f0d59bb0c3cf
Vladislav <vladislav.golubkov2014@yandex.ru>
2026-01-18 22:10:38 +0500

    hot fixes

--- src/processing.py
+++ src/processing.py
@@ -1,13 +1,17 @@
-def filter_by_states(voc_list, state="EXECUTED"):
+from typing import Union, List, Dict, Any
+
+
+def filter_by_states(voc_list: List[Dict[str, Any]], state: str = "EXECUTED") -> List[Dict[str, Any]]:
     filtered_voc_list = []
     for voc in voc_list:
         if voc[state] == state:
             filtered_voc_list.append(voc)
         else:
             pass
+    return filtered_voc_list
 
 
-def sort_by_date(voc_list, sorting_methode="descending"):
+def sort_by_date(voc_list: List[Dict[str, Any]], sorting_methode: str = "descending") -> List[Dict[str, Any]]:
     sorted_voc_list = []
 
     for voc in voc_list:
@@ -16,9 +20,9 @@ def sort_by_date(voc_list, sorting_methode="descending"):
 
     for voc in voc_list:
         if sorting_methode == "descending":
-            sorted_voc_list = sorted(voc, key=lambda x: x["date"], reverse=True)
+            sorted_voc_list = sorted(voc_list, key=lambda x: x["date"], reverse=True)
         elif sorting_methode == "ascending":
-            sorted_voc_list = sorted(voc, key=lambda x: x["date"], reverse=False)
+            sorted_voc_list = sorted(voc_list, key=lambda x: x["date"], reverse=False)
         else:
             sorted_voc_list = voc_list
     return sorted_voc_list
